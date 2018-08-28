68e3d6c8765fc7c7916335e84be0db8b64215b5e
fdas3213 <szx9404@gmail.com>
2018-08-27 22:10:59 -0700

    finished self join

--- self join.sql
+++ self join.sql
@@ -48,3 +48,55 @@ WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road'
 
 Q7.
 -- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company = b.company AND a.num = b.num)
+  JOIN stops stopa ON (a.stop = stopa.id)
+  JOIN stops stopb ON (b.stop = stopb.id)
+ WHERE stopa.id = 115 AND stopb.id = 137
+ GROUP BY a.num, a.company
+
+
+Q8.
+-- Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company = b.company AND a.num = b.num)
+  JOIN stops stopa ON (a.stop = stopa.id)
+  JOIN stops stopb ON (b.stop = stopb.id)
+ WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+
+
+Q9.
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, 
+-- offered by the LRT company. Include the company and bus no. of the relevant services.
+SELECT stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company = b.company AND a.num = b.num)
+  JOIN stops stopa ON (a.stop = stopa.id)
+  JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart' 
+
+Q10.
+-- Find the routes involving two buses that can go from Craiglockhart to Sighthill.
+-- Show the bus no. and company for the first bus, the name of the stop for the transfer,
+-- and the bus no. and company for the second bus.
+SELECT DISTINCT S.num, S.company, stops.name, E.num, E.company
+FROM
+(SELECT a.company, a.num, b.stop
+ FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+ WHERE a.stop=(SELECT id FROM stops WHERE name= 'Craiglockhart')
+)S
+  JOIN
+(SELECT a.company, a.num, b.stop
+ FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+ WHERE a.stop=(SELECT id FROM stops WHERE name= 'Sighthill')
+)E
+ON (S.stop = E.stop)
+JOIN stops ON(stops.id = S.stop)
+
+
+
+
+
+
